76c5fbafa8f0f9e663dd9bec6bf7c362ead9fbd5
Mike Ghen <mike.ghen@gmail.com>
2017-11-01 21:20:47 -0400

    Second data structure, starting on solving algo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print("Hello world")
+from pprint import pprint
 
-
-
-solution_space=[[ 4, None, 1, 2, 9, None, None, 7, 5],
+solution_space = [[ 4, None, 1, 2, 9, None, None, 7, 5],
 [2, None, None, 3, None, None, 8, None, None],
 [None, 7, None, None,8, None, None, None, 6],
 [None, None, None, 1, None, 3, None, 6, 2],
@@ -12,10 +10,21 @@ solution_space=[[ 4, None, 1, 2, 9, None, None, 7, 5],
 [None, None, 7, None, None, 1, None, None, 4],
 [8, 9, None, None, 6, 5, 1, None, 7]]
 
-#print(solution_space[0])
-# for row in solution_space:
-#     print(row[0])
+L_FULL = {1,2,3,4,5,6,7,8,9}
 
+solving_space = []
 for i in range(9):
+    solving_space.append([])
     for j in range(9):
-        print(solution_space[i][j])
+        solving_space[i].append(L_FULL)
+#pprint(solving_space)
+
+pprint(solving_space[0][1] - set(solution_space[0]) - set([row[1] for row in solution_space]))
+
+# #print(solution_space[0])
+# # for row in solution_space:
+# #     print(row[0])
+#
+# for i in range(9):
+#     for j in range(9):
+#         print(solution_space[i][j])
