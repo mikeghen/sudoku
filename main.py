1694a43aa4db1e0d723aa2137840dc588e9887ab
Mike Ghen <mike.ghen@gmail.com>
2017-11-08 17:05:49 -0500

    Finish Sudoku

--- main.py
+++ main.py
@@ -19,7 +19,37 @@ for i in range(9):
         solving_space[i].append(L_FULL)
 #pprint(solving_space)
 
-pprint(solving_space[0][1] - set(solution_space[0]) - set([row[1] for row in solution_space]))
+def row_set(i):
+    return set(solution_space[i])
+
+def column_set(j):
+    return set([row[j] for row in solution_space])
+
+def ninth_set(i,j):
+    a = 3*(i/3)
+    b = 3*(j/3)
+    return set([solution_space[a][b], solution_space[a][b+1], solution_space[a][b+2], solution_space[a+1][b], solution_space[a+1][b+1], solution_space[a+1][b+2], solution_space[a+2][b], solution_space[a+2][b+1], solution_space[a+2][b+2]])
+
+for k in range(10):
+    is_done = True
+    for i in range(9):
+        for j in range(9):
+            if solution_space[i][j] is not None:
+                solving_space[i][j] = set([solution_space[i][j]])
+            else:
+                solving_space[i][j] = solving_space[i][j] - row_set(i) - column_set(j) - ninth_set(i,j)
+            if len(solving_space[i][j]) == 1:
+                solution_space[i][j] = (solving_space[i][j]).pop()
+            if solution_space[i][j] == None:
+                is_done = False
+    if is_done:
+        print(k)
+        break
+
+pprint(solution_space)
+pprint(solving_space)
+
+
 
 # #print(solution_space[0])
 # # for row in solution_space:
